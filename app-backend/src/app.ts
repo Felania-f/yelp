38266705144a7d4a6a2568fb8320dc6b4793faa3
Karim Debza <bwb-k.debza@fondespierre.com>
2025-04-03 13:57:29 +0200

    ajouter ts et les depend

---
+++ app-backend/src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+// import restaurantRoutes from '../src/routes/restaurant.routes';
+
+dotenv.config();
+
+const app = express();
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Serveur lancé sur le port ${PORT}`));
