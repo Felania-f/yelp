3b19ebfa967ed4d2883be220308358c3be190a8a
Karim Debza <bwb-k.debza@fondespierre.com>
2025-04-03 14:00:20 +0200

    changement de port

--- app-backend/src/app.ts
+++ app-backend/src/app.ts
@@ -8,5 +8,5 @@ dotenv.config();
 const app = express();
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Serveur lancé sur le port ${PORT}`));
